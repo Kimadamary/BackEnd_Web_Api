87ef36945517a0311d8289f5b8c2c092424d5738
loooh.hoy6@gmail.com <loooh.hoy6@gmail.com>
2026-07-16 17:50:57 +0300

    1

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ function setupNavButtons() {
 }
 
 // Замени порт 7145 на свой, если он отличается при запуске Web API!
-const API_BASE_URL = 'https://empty-hairs-invent.loca.lt/api/users';
+const API_BASE_URL = 'https://sharp-bears-dig.loca.lt/api/users';
 
 function openModal(modalId) {
     const modal = document.getElementById(modalId);
